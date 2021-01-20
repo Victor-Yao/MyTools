3f441093ce8826d50fd957b3fa0a54378b41dd15
Victor-Yao <ywfire0620@outlok.com>
2021-01-20 16:17:48 +0800

    add ftp logparser batch

---
+++ LogParser Batch/FTP_LogParser.bat
@@ -0,0 +1,9 @@
+@echo off
+set path=%1
+set logParser="C:\Program Files (x86)\Log Parser 2.2\logparser.exe"
+cd /d %path%
+mkdir ret
+
+for %%n in (u_ex*.log) do %logParser% -i:W3C -q:on -o:csv "SELECT date,time,c-ip,cs-username,s-ip,s-port,cs-method,cs-uri-stem,sc-status,sc-win32-status,sc-substatus,x-session,x-fullpath FROM %%n" >> ".\ret\%%n.csv"
+
+exit
