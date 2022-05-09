9dd509a7dc44776e84a6e60bb4e65969f77f2f81
wei.yao <ywfire0620@outlook.com>
2022-05-09 14:01:38 +0800

    add echo to alert script is processing for FTP script

--- LogParser Batch/FTP_LogParser.bat
+++ LogParser Batch/FTP_LogParser.bat
@@ -4,6 +4,8 @@ set logParser="C:\Program Files (x86)\Log Parser 2.2\logparser.exe"
 cd /d %path%
 mkdir ret
 
+echo "start processing"
 for %%n in (u_ex*.log) do %logParser% -i:W3C -q:on -o:csv "SELECT date,time,c-ip,cs-username,s-ip,s-port,cs-method,cs-uri-stem,sc-status,sc-win32-status,sc-substatus,x-session,x-fullpath FROM %%n" >> ".\ret\%%n.csv"
+echo "start done"
 
 exit
