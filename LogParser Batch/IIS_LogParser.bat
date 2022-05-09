f7107ffff45acadcdb071f201ff4175127fc8832
wei.yao <ywfire0620@outlook.com>
2022-05-09 14:02:35 +0800

    add echo line to alert user script is processing

--- LogParser Batch/IIS_LogParser.bat
+++ LogParser Batch/IIS_LogParser.bat
@@ -4,6 +4,8 @@ set logParser="C:\Program Files (x86)\Log Parser 2.2\logparser.exe"
 cd /d %path%
 mkdir ret
 
+echo "start processing"
 for %%n in (u_ex*.log) do %logParser% -i:iisw3c -q:off -o:csv "SELECT date,time,s-ip,cs-method,cs-uri-stem,cs-uri-query,s-port,cs-username,c-ip,cs(User-Agent), cs(Referer), sc-status,sc-substatus,sc-win32-status,time-taken FROM %%n" >> ".\ret\%%n.csv"
+echo "done"
 
 exit
