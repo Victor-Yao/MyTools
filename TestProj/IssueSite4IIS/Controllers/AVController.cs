e108248d22124a1494d9cd0029491556c8c46449
Victor-Yao <ywfire0620@outlok.com>
2023-07-28 14:59:32 +0800

    add issuesite4iis

---
+++ TestProj/IssueSite4IIS/Controllers/AVController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CrashSite4IIS.Controllers
+{
+    public class AVController : Controller
+    {
+        // GET: Crash
+        [System.Runtime.InteropServices.DllImport("kernel32.dll")]
+        private static extern void RtlZeroMemory(IntPtr dest, IntPtr size);
+
+        public ActionResult Index()
+        {
+            RtlZeroMemory(new IntPtr(-1), new IntPtr(-1));
+            return View();
+        }
+    }
+}
---
+++ TestProj/IssueSite4IIS/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CrashSite4IIS.MvcApplication" Language="C#" %>
